ed50cc698d33240e4fee30f5903c96c16a856dde
zqi <zq123456>
2019-11-06 10:41:28 +0800

    初始化程序，采用maven管理包

---
+++ src/main/java/com/leraning/maven/HelloTest.java
@@ -0,0 +1,14 @@
+package com.leraning.maven;
+
+import org.junit.Test;
+
+/**
+ * @author zouqi
+ * @date 2019/11/6
+ */
+public class HelloTest {
+    @Test
+    public void say(){
+        System.out.println("say hello~");
+    }
+}
