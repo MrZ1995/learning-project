b9e34e1155ddb97cacfebe301de7d9d7c7e39753
86137 <86137@LAPTOP-IVHKCAB5>
2019-11-10 19:07:07 +0800

    基础练习

---
+++ first-year/src/main/java/com/learning/maven/basic-concepts-2/data-type-seal/JavaDemo2.java
@@ -0,0 +1,8 @@
+public class JavaDemo2{
+	public static void main(String args[]) {
+		//int变量=常见变量
+		int x=10;
+		//int型变量*int型变量=int型数据
+		System.out.println(x*x);
+	}
+}
---
+++ first-year/src/main/java/com/learning/maven/basic-concepts-2/program-annotation/Javademo2.java
@@ -0,0 +1,8 @@
+
+public class Javademo2 {
+	public static void main(String args[]) {
+		int ÄêÁä=20£»
+				System.out.println(ÄêÁä)£»
+	}
+
+}
