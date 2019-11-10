006a298e0d491ec228cc646d2dd9a2afe6d22a3e
86137 <86137@LAPTOP-IVHKCAB5>
2019-11-10 19:07:38 +0800

    基础练习

---
+++ first-year/src/main/java/com/learning/maven/basic-concepts-2/data-type-seal/JavaDemo.java
@@ -0,0 +1,7 @@
+public class JavaDwmo{
+	public static void main(String args[)]{
+		int num ;
+		num=10;
+		System.out.println(num);
+	}
+}
---
+++ first-year/src/main/java/com/learning/maven/basic-concepts-2/program-annotation/JavaDemo.java
@@ -0,0 +1,5 @@
+public class JavaDemo{
+	public static void main(String args[]) {
+		System.out.println("www.mldn.cn"):
+	}
+}
