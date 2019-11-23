fa4df59edfd792dc8b56e607dd027440e0cb9bdc
zoude162 <901602zd>
2019-11-23 22:47:49 +0800

    测试

--- first-year/src/main/java/com/learning/maven/basic_concepts_2/data_type_seal/JavaDemo.java
+++ first-year/src/main/java/com/learning/maven/basic_concepts_2/data_type_seal/JavaDemo.java
@@ -3,6 +3,7 @@ package com.learning.maven.basic_concepts_2.data_type_seal;
 public class JavaDemo{
 	public static void main(String args[]){
 		int num ;
+		// 注释
 		num=10;
 		System.out.println(num);
 	}
