86df3675f03ad46402751f403ad72ab82cb66960
zoude162 <901602zd>
2019-11-28 20:33:36 +0800

    对象数组动态初始化

--- first-year/src/main/java/com/learning/maven/array/ObjectArray.java
+++ first-year/src/main/java/com/learning/maven/array/ObjectArray.java
@@ -1,27 +1,19 @@
 package com.learning.maven.array;
 
-import com.learning.maven.class_and_object.Person;
-//class Person{
-//    private String name;
-//    private int age;
-//    public Person(String name,int age){
-//        this.name=name;
-//        this.age=age;
-//    }
-//    public String getInfo(){
-//        return "姓名:" +this.name+"、年龄:" + this.age;
-//    }
-//}
-//public class ObjectArray {
-//    public static void main(String args[]){
-//        Person per[] = new Person[]{
-//                new Person("张三",20),new Person("李四",18),
-//                new Person("王五",19)};
-//
-//        for(int x =0;x<per.length;x++)
-//
-//    {
-//        System.out.println(per[x].getInfo());
-//    }
-//    }
-//}
+import com.learning.maven.array.Person;
+
+
+
+public class ObjectArray {
+    public static void main(String args[]) {
+        Person per[] = new Person[] {
+                new Person("张三", 20), new Person("李四", 18),
+                new Person("王五", 19) }; 	// 对象数组静态初始化
+        for (int x = 0; x < per.length; x++) {	// 循环输出
+            System.out.println(per[x].getInfo());
+        }
+    }
+}
+
+
+
--- first-year/src/main/java/com/learning/maven/array/ObjectArray2.java
+++ first-year/src/main/java/com/learning/maven/array/ObjectArray2.java
@@ -13,16 +13,16 @@ class Person {
     }
 
 }
-    public class ObjectArray2 {
-        public static void main(String args[]) {
-            Person per[] = new Person[3];    //
-            per[0] = new Person("张三", 20);    //
-            per[1] = new Person("李四", 18);    //
-            per[2] = new Person("王五", 19);    //
-            for (int x = 0; x < per.length; x++) {// 循环输出
-                System.out.println(per[x].getInfo());
-            }
+public class ObjectArray2 {
+    public static void main(String args[]) {
+        Person per[] = new Person[] {
+                new Person("张三", 20), new Person("李四", 18),
+                new Person("王五", 19) }; 	// 对象数组静态初始化
+        for (int x = 0; x < per.length; x++) {	// 循环输出
+            System.out.println(per[x].getInfo());
         }
     }
+}
+
 
 
