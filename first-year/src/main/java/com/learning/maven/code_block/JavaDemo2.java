cca628dae86c1840850d2767e77eed3cf528482e
zoude162 <901602zd>
2019-11-30 11:04:40 +0800

    定义普通代码块

---
+++ first-year/src/main/java/com/learning/maven/code_block/JavaDemo2.java
@@ -0,0 +1,12 @@
+package com.learning.maven.code_block;
+
+public class JavaDemo2 {
+    public static void main(String args[]) {
+        {
+            int x = 10;
+            System.out.println("x=" + x);
+        }
+        int x = 100;
+        System.out.println("x=" + x);
+    }
+}
