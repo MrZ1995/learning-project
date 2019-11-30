0df06c4f41b37990ddd357474a010c9c006d460c
zoude162 <901602zd>
2019-11-30 10:55:58 +0800

    普通代码块

---
+++ first-year/src/main/java/com/learning/maven/code_block/JavaDemo1.java
@@ -0,0 +1,13 @@
+package com.learning.maven.code_block;
+
+public class JavaDemo1 {
+    public static void main(String args[]){
+        if(true){
+            int x  = 10;
+            System.out.println("x="+x);
+
+    }
+    int x =100;
+    System.out.println("x="+x);
+}
+}
