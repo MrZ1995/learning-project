f4f70fe0c46fe63a6d825482c7b2a7dd43d4afa8
zoude162 <901602zd>
2019-11-30 10:20:37 +0800

    static应用案例

--- first-year/src/main/java/com/learning/maven/class_and_object/Static.java
+++ first-year/src/main/java/com/learning/maven/class_and_object/Static.java
@@ -1,4 +1,19 @@
 package com.learning.maven.class_and_object;
-//
-//public class Static {
-//}
+class Book {
+    private String title;
+    private static int count = 0;
+    public Book(String title){
+        this.title = title;
+        count++;
+        System.out.println("第"+count+"本图书创建出来。");
+    }
+}
+public class Static {
+    public static void  main(String args[]){
+        new Book("Java开发实战经典");
+        new Book("Spring实战开发");
+        new Book("Spring微结构实战开发");
+
+    }
+
+}
