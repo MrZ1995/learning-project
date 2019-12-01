dfb5aa9fddf230445a30f222440dc727308ea742
zoude162 <901602zd>
2019-12-01 09:54:08 +0800

    定义构造代码块

---
+++ first-year/src/main/java/com/learning/maven/code_block/JavaDemo3.java
@@ -0,0 +1,16 @@
+package com.learning.maven.code_block;
+class Person {
+    public Person() {
+        System.out.println("【构造方法】Person类方法构造执行");
+    }
+
+    {
+        System.out.println("【构造块)Person构造块执行");
+    }
+}
+public class JavaDemo3 {
+    public static void main(String args[]){
+        new Person();
+        new Person();
+    }
+}
