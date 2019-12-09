d721b5dc60e9f8ba5c7909774863d28fd4ceb647
zoude162 <901602zd>
2019-12-09 20:18:16 +0800

    利用equals()方法实现字符串内容比较

---
+++ first-year/src/main/java/com/learning/maven/string_class/StringClass3.java
@@ -0,0 +1,10 @@
+package com.learning.maven.string_class;
+
+public class StringClass3{
+    public static void main(String args[]){
+        String strA = "mldn";
+        String strB = new String("mldn");
+        System.out.println(strA.equals(strB));
+    }
+
+}
