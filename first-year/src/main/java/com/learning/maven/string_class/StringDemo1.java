024d2e8b8de836066ff6fce04058d599aa9ff6ac
zoude162 <901602zd>
2019-12-09 20:29:24 +0800

    观察字符串匿名对象

--- first-year/src/main/java/com/learning/maven/string_class/StringDemo1.java
+++ first-year/src/main/java/com/learning/maven/string_class/StringDemo1.java
@@ -1,12 +1,12 @@
 package com.learning.maven.string_class;
 
-public class StringDemo1 {
-    public static void main(String argss[]){
-        String strA = "mldn";
-        String strB = new String("mldn");
-        String strC =strB;
-        System.out.println(strA == strB);
-        System.out.println(strA == strC);
-        System.out.println(strC == strB);
-    }
-}
+//public class StringDemo1 {
+//    public static void main(String argss[]){
+//        String strA = "mldn";
+//        String strB = new String("mldn");
+//        String strC =strB;
+//        System.out.println(strA == strB);
+//        System.out.println(strA == strC);
+//        System.out.println(strC == strB);
+//    }
+//}
