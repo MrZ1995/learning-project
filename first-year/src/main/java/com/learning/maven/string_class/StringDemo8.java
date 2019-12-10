14189be14faa36b2754f21d2b4bde46491221784
zoude162 <901602zd>
2019-12-10 16:02:00 +0800

    通过代码分析字串符对象池操作

---
+++ first-year/src/main/java/com/learning/maven/string_class/StringDemo8.java
@@ -0,0 +1,13 @@
+package com.learning.maven.string_class;
+
+public class StringDemo8 {
+    public static void main(String args[]) {
+        String strA = "mldn";
+        String strB = "mldnjaca";
+        String strC = "mldn";
+        System.out.println(strA == strB);
+        System.out.println(strA == strC);
+
+    }
+}
+
