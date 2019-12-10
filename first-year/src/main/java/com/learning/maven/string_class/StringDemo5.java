fe7a0c9500206efa201caa15264a9e0c38bfe259
zoude162 <901602zd>
2019-12-10 15:17:21 +0800

    观察问题

---
+++ first-year/src/main/java/com/learning/maven/string_class/StringDemo5.java
@@ -0,0 +1,10 @@
+package com.learning.maven.string_class;
+
+public class StringDemo5 {
+    public static void main(String args[]){
+        String input = null;
+        if (input.equals("tootk")){
+            System.out.println("www.yootk.com");
+        }
+    }
+}
