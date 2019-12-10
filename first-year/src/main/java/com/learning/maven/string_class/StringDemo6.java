18e4537b9e29fdf4e9e30147d06a3631eafb4e3f
zoude162 <901602zd>
2019-12-10 15:25:07 +0800

    回避NullPointerException问题

---
+++ first-year/src/main/java/com/learning/maven/string_class/StringDemo6.java
@@ -0,0 +1,10 @@
+package com.learning.maven.string_class;
+
+public class StringDemo6 {
+    public static void main(String args[]){
+        String input = null;
+        if ("yootk".equals(input)){
+            System.out.println("www.tootk.com");
+        }
+    }
+}
