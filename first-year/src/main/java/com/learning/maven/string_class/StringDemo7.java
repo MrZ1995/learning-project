b574447a8f2ba51642abafd5ee0f32072037b698
zoude162 <901602zd>
2019-12-10 15:54:01 +0800

    观察直接赋值时的堆内存自动引用

---
+++ first-year/src/main/java/com/learning/maven/string_class/StringDemo7.java
@@ -0,0 +1,14 @@
+package com.learning.maven.string_class;
+
+public class StringDemo7 {
+    public static void main(String args[]) {
+
+
+        String strA = "mldn";
+        String strB = "mldn";
+        String strC = "tootk";
+        System.out.println(strA == strB);
+        System.out.println(strA == strC);
+
+    }
+}
