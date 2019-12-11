934a241881c8842440e4d4bd9ae0c3659587e4db
zoude162 <901602zd>
2019-12-11 19:48:48 +0800

    构造方法实例化String类对象不自动入池保存和手动保存

---
+++ first-year/src/main/java/com/learning/maven/string_class/StringDemo9.java
@@ -0,0 +1,9 @@
+package com.learning.maven.string_class;
+
+public class StringDemo9 {
+    public static void main(String args[]){
+        String strA = new String("mldn");
+        String strB = "mldn";
+        System.out.println(strA==strB);
+    }
+}
