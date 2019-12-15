31843d7526b86cb32c525e3136e7ea588dd8799d
zoude162 <901602zd>
2019-12-15 19:49:08 +0800

    静态常量池

---
+++ first-year/src/main/java/com/learning/maven/string_class/StringDemo11.java
@@ -0,0 +1,9 @@
+package com.learning.maven.string_class;
+
+public class StringDemo11 {
+    public static void main(String args[]){
+        String strA = "www.yootk.com";
+        String strB = "www."+"yootk"+".com";
+        System.out.println(strA==strB);
+    }
+}
