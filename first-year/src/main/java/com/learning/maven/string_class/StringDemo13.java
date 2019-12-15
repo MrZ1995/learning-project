8f425b16b293015a5a793742d438d3073ad8ad41
zoude162 <901602zd>
2019-12-15 20:23:37 +0800

    观察字符串修改

---
+++ first-year/src/main/java/com/learning/maven/string_class/StringDemo13.java
@@ -0,0 +1,10 @@
+package com.learning.maven.string_class;
+
+public class StringDemo13 {
+    public static void main(String args[]){
+        String str ="www.";
+        str += "mldn";
+        str = str + ".cn";
+        System.out.println(str);
+    }
+}
